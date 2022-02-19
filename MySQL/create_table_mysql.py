86479f8bddf379dcbce5cc9048c6da980c9bcecc
Shih Chieh Hsiao <mickyhsiao03@hotmail.com>
2022-02-18 17:47:19 -0800

    ignore the javascript files, added mysql create table

---
+++ MySQL/create_table_mysql.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+db_conn = mysql.connector.connect(host="localhost", user="root", 
+password="2705895a", database="acit3495")
+
+db_cursor = db_conn.cursor()
+
+db_cursor.execute('''
+          CREATE TABLE IF NOT EXISTS student_grades
+          (id INT NOT NULL AUTO_INCREMENT, 
+           first_name VARCHAR(250) NOT NULL,
+           last_name VARCHAR(100) NOT NULL,
+           course_name VARCHAR(50) NOT NULL,
+           grade INT NOT NULL,
+           CONSTRAINT student_grades_pk PRIMARY KEY (id))
+          ''')
+db_conn.commit()
+db_conn.close()
